ac4edbb7c965206076d3873e3106c1105a0fcea5
Ben Kamphaus <ben.kamphaus@gmail.com>
2022-11-20 09:44:21 -0700

    wip python progress fixing template

--- scripts/fixup.py
+++ scripts/fixup.py
@@ -11,11 +11,12 @@ cnv1 = "processed/cnv_ref_fixed_1.tsv"
 cnv2 = "processed/cnv_ref_fixed_3.csv"
 var1 = "processed/variant_ref_21.tsv"
 var2 = "processed/variant_ref_32.tsv"
-
+var3 = "processed/variants.txt"
 cnv1_df = pd.read_csv(cnv1, sep='\t')
 cnv2_df = pd.read_csv(cnv2, sep='\t')
 var1_df = pd.read_csv(var1, sep='\t')
 var2_df = pd.read_csv(var2, sep='\t')
+var3_df = pd.read_csv(var3, sep='\t')
 
 # get list of current gene names
 genes = "/Users/bkamphaus/code/pret/seed_data/raw/genes/hgnc_complete_set.txt"
@@ -29,6 +30,10 @@ cnv2_df['Genes'] = cnv2_df['Genes'].apply(partial(remap_multi, hugos))
 # remove from var ref files genes not in hugos
 keep_var1_df = var1_df[~((var1_df['variable'] == 'Hugo_Symbol') & (var1_df['value'].apply(lambda x: x not in hugos)))]
 keep_var2_df = var2_df[~((var2_df['variable'] == 'Hugo_Symbol') & (var2_df['value'].apply(lambda x: x not in hugos)))]
+
+# var3 in a different format
+# drop if not here
+# fix: var3_df['hugo'] = cnv2_df['hugo']
 
 # write fixed files
 keep_var1_df.to_csv(var1, sep='\t', header=True)
